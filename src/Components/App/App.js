ff93ccf5b026902d9d98cfc163d5f41655e88512
Trent Ireland <trentireland87@gmail.com>
2023-08-11 16:02:37 +0100

    deleted files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
@@ -8,6 +8,16 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [playlistName, setPlaylistName] = useState('My Playlist');
   const [playlistTracks, setPlaylistTracks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [accessToken, setAccessToken] = useState(null);
+  const [tokenExpiry, setTokenExpiry] = useState(null);
+
+  useEffect(() => {
+    // Check if token is expired
+    if (Date.now() > tokenExpiry) {
+      // Refresh the token here
+    }
+  }, [accessToken, tokenExpiry]);
 
   const addTrack = track => {
     if (playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
@@ -24,23 +34,43 @@ function App() {
     setPlaylistName(name);
   };
 
+  const handleSearch = searchTerm => {
+    setLoading(true);
+    // Fetch tracks from Spotify API
+    // Filter out tracks that are already in the playlist
+    // Set the search results
+    setLoading(false);
+  };
+
+  const savePlaylist = () => {
+    setLoading(true);
+    // Save the playlist to Spotify
+    setLoading(false);
+  };
+
   return (
     <div>
       <h1>
         Ja<span className="highlight">mmm</span>ing
       </h1>
       <div className="App">
-        <SearchBar setSearchResults={setSearchResults} />
-        <div className="App-playlist">
-          <SearchResults searchResults={searchResults} onAdd={addTrack} />
-          <Playlist
-            playlistName={playlistName}
-            onNameChange={updatePlaylistName}
-            playlistTracks={playlistTracks}
-            onRemove={removeTrack}
-            onAdd={addTrack}
-          />
-        </div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <SearchBar onSearch={handleSearch} />
+            <div className="App-playlist">
+              <SearchResults searchResults={searchResults} onAdd={addTrack} />
+              <Playlist
+                playlistName={playlistName}
+                onNameChange={updatePlaylistName}
+                playlistTracks={playlistTracks}
+                onRemove={removeTrack}
+                onSave={savePlaylist}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
